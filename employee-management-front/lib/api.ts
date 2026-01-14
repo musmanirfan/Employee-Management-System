fe5f20cf1938c18c4292dde8433a175041459d57
musmanirfan <usmanmala356@gmail.com>
2026-01-14 22:28:38 +0500

    data updated

--- employee-management-front/lib/api.ts
+++ employee-management-front/lib/api.ts
@@ -16,23 +16,3 @@ export async function apiSend<T>(path: string, method: 'POST' | 'PUT' | 'DELETE'
     return res.json().catch(() => ({} as T));
 }
 
-
-const BASE2 = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-
-export async function apiGet1<T>(path: string): Promise<T> {
-    const res1 = await fetch(`${BASE2}, ${path}`, { cache: 'no-store' });
-    if (!res1.ok) throw new Error(await res1.text());
-    return res1.json();
-}
-
-
-export async function apiSend1<T>(path: string, method: 'POST' | 'PUT' | 'DELETE', body?: unknown): Promise<T> {
-    const res1 = await fetch(`${BASE2}, ${path}`, {
-        method,
-        headers: { 'Content-Type': 'application/json' },
-        body: body ? JSON.stringify(body) : undefined
-    });
-    if (!res1.ok) throw new Error(await res1.text());
-    return res1.json().catch(()=>({} as T));
-}
